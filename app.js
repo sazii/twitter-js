1048504cce9099df5e63350467ddcc5921be9545
Saziye Toksari <stoksari@gmail.com>
2017-11-12 19:56:32 +0000

    intro to express server

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const morgan=require("morgan")
+
+var logger=morgan("dev");
+
+/*app.use(function(req,res,next){
+    console.log(req.path,req.method,res.statusCode);
+    next();
+});*/
+
+app.use(logger);
+
+app.listen(8080,function(){
+    console.log("listening on port 8080!'");
+});
+
+app.get('/', function(req,res,next){
+    res.send("welcome!");
+    //next();
+});
+
+
